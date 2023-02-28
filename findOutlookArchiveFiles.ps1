5a7001b671dcb4367407a95d1b4d721d6ce30076
cEricL <cericlindquist@gmail.com>
2023-02-27 21:19:35 -0800

    notes added to findOutlookArchiveFiles.ps1

--- findOutlookArchiveFiles.ps1
+++ findOutlookArchiveFiles.ps1
@@ -1,4 +1,9 @@
 # search for OST or PST files on all local drives
+# click Start, search for PowerShell ISE and launch it
+# copy the following lines and paste into the blue field in PowerShell ISE
+# After pasting, hit "Enter" on the keyboard to run the script
+# output will include a file name and path if any are found
+# make a note of any results for further research to determine whether this is the location for missing email
 $driveListAll = Get-PSDrive ;
 $driveListToCheck = $driveListAll | Where-Object {$_.Name -match '^[(?!)a-z]$'}
 $driveListToCheck | ForEach-Object {
