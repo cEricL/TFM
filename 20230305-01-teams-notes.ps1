b80ff0324862540a208bcbc6810e8810a7799d02
cEricL <cericlindquist@gmail.com>
2023-03-05 16:18:56 -0800

    20230305 notes

---
+++ 20230305-01-teams-notes.ps1
@@ -0,0 +1,30 @@
+<# 20230305-01-teams-notes.ps1
+#>
+
+<# Trent questions on where to focus
+* Goal is to be financially
+* what to do for longer term with general overview
+* get good with Azure or Generalized tech
+* powershell
+#>
+
+<# specific mgmt points for potential value adds
+* Project Mgmt 
+    * esp. Agile
+        * Agile certs/training
+* Architecture 
+    * leveled up tech/ops
+
+* one can be stronger than the other but need noteworthy capacity for both. 
+#>
+
+<# RACI
+Responsible
+Accountable
+Consulted
+Informed
+#>
+
+<# 
+
+#>
